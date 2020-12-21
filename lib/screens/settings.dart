2cb1700d7cdd59b439f2e7b266f82a26b0f28825
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2020-12-22 00:53:03 +0900

    캐시 비우기/설정 초기화 전 경고팝업 추가

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -142,9 +142,8 @@ class _SettingsPageState extends State<SettingsPage> {
                   ListTile(
                     title: Text('화면 순서 변경'),
                     onTap: () async {
-                      Navigator.of(context).push<void>(MaterialPageRoute(
-                          builder: (_) => ChangeOrderPage()
-                      ));
+                      Navigator.of(context).push<void>(
+                          MaterialPageRoute(builder: (_) => ChangeOrderPage()));
                     },
                   ),
                   SwitchListTile(
@@ -185,26 +184,80 @@ class _SettingsPageState extends State<SettingsPage> {
                   ListTile(
                     title: Text('캐시 비우기'),
                     onTap: () async {
-                      Cache().clear();
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('캐시를 비웠습니다.'),
-                        duration: const Duration(seconds: 3),
-                      ));
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: new Text("캐시를 비울까요?"),
+                            content: new Text(
+                                "캐시는 앱에서 알아서 관리하기 때문에 다른 문제가 없다면 굳이 비울 필요가 없습니다.\n"
+                                "캐시의 용량은 대개 0.5MB 이내로 비우더라도 용량 확보에 거의 도움이 되지 않으며, "
+                                "자주 비울 경우 같은 데이터를 반복 요청하게 되므로 네트워크 사용량이 늘어날 수 있습니다.\n"
+                                "캐시를 비우면 다음 실행 시까지 오프라인에서 앱을 이용하실 수 없게 됩니다."),
+                            actions: <Widget>[
+                              new FlatButton(
+                                child: new Text("아니요"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              new FlatButton(
+                                child: new Text("네"),
+                                onPressed: () {
+                                  Cache().clear();
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text('캐시를 비웠습니다.'),
+                                    duration: const Duration(seconds: 3),
+                                  ));
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                   ),
                   ListTile(
                     title: Text('설정 초기화'),
                     onTap: () async {
-                      setState(() {
-                        _prefs = new Prefs.defaultValue();
-                        SharedPrefs().push(_prefs);
-                      });
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('설정을 초기화했습니다.'),
-                        duration: const Duration(seconds: 3),
-                      ));
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: new Text("정말 초기화할까요?"),
+                            content: new Text("모든 설정이 기본값으로 돌아가며, 복구할 수 없습니다."),
+                            actions: <Widget>[
+                              new FlatButton(
+                                child: new Text("아니요"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              new FlatButton(
+                                child: new Text("네"),
+                                onPressed: () {
+                                  setState(() {
+                                    _prefs = new Prefs.defaultValue();
+                                    SharedPrefs().push(_prefs);
+                                  });
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text('설정을 초기화했습니다.'),
+                                    duration: const Duration(seconds: 3),
+                                  ));
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                   ),
                 ],
