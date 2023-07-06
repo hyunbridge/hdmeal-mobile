8fbb3febd14f75310ca64dea3c01b24033525842
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2023-07-06 17:03:23 +0900

    API에서 null값 받았을 때 섹션단위로 예외처리하도록 개선

--- lib/widgets/sections.dart
+++ lib/widgets/sections.dart
@@ -54,7 +54,58 @@ List<Widget> menuSection({
   required bool enableKeywordHighlight,
   required List<String> highlightedKeywords,
 }) {
-  if (menu.length == 0) {
+  try {
+    if (menu.length == 0) {
+      throw "식단 정보가 없습니다.";
+    }
+
+    return [
+      ListTile(
+        title: Text(
+          "급식",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: Transform.translate(
+          offset: Offset(16, 0),
+          child: Visibility(
+            child: IconButton(
+                icon: const Icon(Icons.share),
+                color: Theme.of(context).textTheme.bodyLarge?.color,
+                onPressed: () {
+                  Share.share(
+                      "<${date.month}월 ${date.day}일(${_weekday[date.weekday]})>\n${menu.map((e) => e[0]).join(",\n")}");
+                }),
+            visible: !kIsWeb,
+          ),
+        ),
+      ),
+      ...menu.map((e) => ListTile(
+            title: Text(
+              e[0],
+              style: enableKeywordHighlight &&
+                      _checkKeyword(e[0], highlightedKeywords)
+                  ? TextStyle(fontWeight: FontWeight.bold)
+                  : null,
+            ),
+            subtitle: e[1].length > 0 && showAllergyInfo
+                ? Text(
+                    e[1].map((n) => _allergyString[n]).join(", "),
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  )
+                : null,
+            visualDensity: VisualDensity(vertical: -4),
+            onTap: () async {
+              launch(context,
+                  "https://www.google.com/search?q=${Uri.encodeComponent(e[0])}&tbm=isch");
+            },
+          ))
+    ];
+  } catch (e) {
     return [
       ListTile(
         title: Text(
@@ -66,57 +117,11 @@ List<Widget> menuSection({
         ),
       ),
       ListTile(
-        title: Text("식단정보가 없습니다."),
+        title: Text(menu.length == 0 ? "식단 정보가 없습니다." : "식단 정보를 불러올 수 없습니다."),
         visualDensity: VisualDensity(vertical: -4),
       ),
     ];
   }
-  return [
-    ListTile(
-      title: Text(
-        "급식",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      trailing: Transform.translate(
-        offset: Offset(16, 0),
-        child: Visibility(
-          child: IconButton(
-              icon: const Icon(Icons.share),
-              color: Theme.of(context).textTheme.bodyLarge?.color,
-              onPressed: () {
-                Share.share(
-                    "<${date.month}월 ${date.day}일(${_weekday[date.weekday]})>\n${menu.map((e) => e[0]).join(",\n")}");
-              }),
-          visible: !kIsWeb,
-        ),
-      ),
-    ),
-    ...menu.map((e) => ListTile(
-          title: Text(
-            e[0],
-            style: enableKeywordHighlight &&
-                    _checkKeyword(e[0], highlightedKeywords)
-                ? TextStyle(fontWeight: FontWeight.bold)
-                : null,
-          ),
-          subtitle: e[1].length > 0 && showAllergyInfo
-              ? Text(
-                  e[1].map((n) => _allergyString[n]).join(", "),
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                )
-              : null,
-          visualDensity: VisualDensity(vertical: -4),
-          onTap: () async {
-            launch(context,
-                "https://www.google.com/search?q=${Uri.encodeComponent(e[0])}&tbm=isch");
-          },
-        ))
-  ];
 }
 
 List<Widget> timetableSection(
@@ -125,26 +130,39 @@ List<Widget> timetableSection(
     required int userClass,
     required List timetable,
     required void Function() onTap}) {
-  if (timetable.length == 0) {
-    timetable.add("시간표 정보가 없습니다.");
-  }
-
-  return [
-    ListTile(
-      title: Text(
-        "$userGrade학년 $userClass반 시간표",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
+  final header = ListTile(
+    title: Text(
+      "$userGrade학년 $userClass반 시간표",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
       ),
-      onTap: onTap,
     ),
-    ...timetable.map((e) => ListTile(
-          title: Text(e),
-          visualDensity: VisualDensity(vertical: -4),
-        ))
-  ];
+    onTap: onTap,
+  );
+
+  try {
+    if (timetable.length == 0) {
+      throw "시간표 정보가 없습니다.";
+    }
+
+    return [
+      header,
+      ...timetable.map((e) => ListTile(
+            title: Text(e),
+            visualDensity: VisualDensity(vertical: -4),
+          ))
+    ];
+  } catch (_) {
+    return [
+      header,
+      ListTile(
+        title:
+            Text(timetable.length == 0 ? "시간표 정보가 없습니다." : "시간표를 불러올 수 없습니다."),
+        visualDensity: VisualDensity(vertical: -4),
+      )
+    ];
+  }
 }
 
 List<Widget> scheduleSection(
@@ -170,27 +188,34 @@ List<Widget> scheduleSection(
       visualDensity: VisualDensity(vertical: -4),
     ));
   } else {
-    schedule.forEach((element) {
-      if (showMyScheduleOnly) {
-        if (element[1].length == 0 || element[1].contains(userGrade)) {
+    try {
+      schedule.forEach((element) {
+        if (showMyScheduleOnly) {
+          if (element[1].length == 0 || element[1].contains(userGrade)) {
+            section.add(ListTile(
+              title: Text(element[0]),
+              visualDensity: VisualDensity(vertical: -4),
+            ));
+          }
+        } else {
           section.add(ListTile(
             title: Text(element[0]),
             visualDensity: VisualDensity(vertical: -4),
+            subtitle: Text(
+              '${element[1].join("학년, ")}학년',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
           ));
         }
-      } else {
-        section.add(ListTile(
-          title: Text(element[0]),
-          visualDensity: VisualDensity(vertical: -4),
-          subtitle: Text(
-            '${element[1].join("학년, ")}학년',
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          ),
-        ));
-      }
-    });
+      });
+    } catch (_) {
+      section.add(ListTile(
+        title: Text("학사일정을 불러올 수 없습니다."),
+        visualDensity: VisualDensity(vertical: -4),
+      ));
+    }
   }
 
   return section;
